6603752d47381bd07bef8057441834aa0130b304
Leonardo Lai <leonardo.lai@live.com>
2021-03-18 12:22:49 +0100

    restore test-retroactive
    linting

--- run_test.py
+++ run_test.py
@@ -3,7 +3,7 @@
 """
 Simple runner for test.sh but it modifies it to explicily test python2 and 3
 """
-from __future__ import print_function,unicode_literals
+from __future__ import print_function, unicode_literals
 import sys
 import os
 import shutil
@@ -14,7 +14,7 @@ if sys.version_info[0] <= 2:
 
 
 # Build a dead-simple CLI. Not worth argparse, etc
-help="""\
+help = """\
 Run tests with specific versions
 
     $ ./run_test.py # Both Python 2 and 3
@@ -23,88 +23,79 @@ Run tests with specific versions
 
 Any argument specified will be appended to the git-fat shebang. For example
 
-    $ ./run_test.py 2.6
+    $ ./run_test.py 2.7
 
-will change the shebang to 
-    
-    #!/usr/bin/env python2.6
+will change the shebang to
 
-Or specify more than one:
-
-    $ ./run_test.py 2 3 2.6
-
+    #!/usr/bin/env python2.7
 """
 vers = sys.argv[1:]
 if len(vers) == 0:
-    vers = ['2','3']
+    vers = ["2", "3"]
 
-if '-h' in vers or '--help' in vers:
+if "-h" in vers or "--help" in vers:
     print(help)
     sys.exit()
-    
+
 for ver in vers:
-    print('-='*20)
-    print('Testing %s' % ver)
-    print('-_'*20)
-    
-    testdir = 'TEST_py%s' % ver
+    print("-=" * 20)
+    print("Testing %s" % ver)
+    print("-_" * 20)
+
+    testdir = "TEST_py%s" % ver
     testdir = os.path.abspath(testdir)
-    
+
     # Delete the prior test dir and make a new one
     if os.path.isdir(testdir):
         shutil.rmtree(testdir)
     os.makedirs(testdir)
-    
-    shebang = '#!/usr/bin/env python%s\n' % ver
-    pathline = 'export PATH=%s:$PATH\n' % testdir
-    
-    testfile = os.path.join(testdir,'test%s.sh' % ver)
-    testfileR = os.path.join(testdir,'test-retroactive%s.sh' % ver)
-    fatfile = os.path.join(testdir,'git-fat')
-    
+
+    shebang = "#!/usr/bin/env python%s\n" % ver
+    pathline = "export PATH=%s:$PATH\n" % testdir
+
+    testfile = os.path.join(testdir, "test%s.sh" % ver)
+    testfileR = os.path.join(testdir, "test-retroactive%s.sh" % ver)
+    fatfile = os.path.join(testdir, "git-fat")
+
     # Write the files. Do not use multiple with's to support 2.6
-    with open('git-fat','rt') as infile:
-        with open(fatfile,'wt') as outfile:
-            infile.readline() # Skip shebang
+    with open("git-fat", "rt") as infile:
+        with open(fatfile, "wt") as outfile:
+            infile.readline()  # Skip shebang
             outfile.write(shebang)
             outfile.write(infile.read())
-    
-    with open('test.sh','rt') as infile:
-        with open(testfile,'wt') as outfile:
-            outfile.write(infile.readline()) # copy shebang
+
+    with open("test.sh", "rt") as infile:
+        with open(testfile, "wt") as outfile:
+            outfile.write(infile.readline())  # copy shebang
             outfile.write(pathline)
             outfile.write(infile.read())
 
-    with open('test-retroactive.sh','rt') as infile:
-        with open(testfileR,'wt') as outfile:
-            outfile.write(infile.readline()) # copy shebang
+    with open("test-retroactive.sh", "rt") as infile:
+        with open(testfileR, "wt") as outfile:
+            outfile.write(infile.readline())  # copy shebang
             outfile.write(pathline)
             outfile.write(infile.read())
-        
+
     os.chmod(fatfile, 509)
     os.chmod(testfile, 509)
     os.chmod(testfileR, 509)
-    
+
     try:
-        subprocess.check_call(['./test%s.sh' % ver],cwd=testdir)
+        subprocess.check_call(["./test%s.sh" % ver], cwd=testdir)
     except subprocess.CalledProcessError as err:
-        print('F'*60)
-        print(err,file=sys.stderr)
-        print('FAILED python %s'%ver,file=sys.stderr)
-        sys.exit(1)
-    
-    print('###################')
-    print('###### RETRO ######')
-    print('###################')
-    
-    try:
-        subprocess.check_call(['./test-retroactive%s.sh' % ver],cwd=testdir)
-    except subprocess.CalledProcessError as err:
-        print('F'*60)
-        print(err,file=sys.stderr)
-        print('FAILED RETRO python %s'%ver,file=sys.stderr)
+        print("F" * 60)
+        print(err, file=sys.stderr)
+        print("FAILED python %s" % ver, file=sys.stderr)
         sys.exit(1)
 
+    print("###################")
+    print("###### RETRO ######")
+    print("###################")
 
-
-
+    try:
+        subprocess.check_call(["./test-retroactive%s.sh" % ver], cwd=testdir)
+    except subprocess.CalledProcessError as err:
+        print("F" * 60)
+        print(err, file=sys.stderr)
+        print("FAILED RETRO python %s" % ver, file=sys.stderr)
+        sys.exit(1)
